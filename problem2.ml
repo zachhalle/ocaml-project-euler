a4069f9dda16da0b3ac39a4dba6a723084a1a75d
Zachary Halle <zhalle@cmu.edu>
2015-08-07 17:11:41 -0400

    added problem 2

--- problem2.ml
+++ problem2.ml
@@ -3,8 +3,8 @@ open Printf
 let main () =
   let a, b = ref 1, ref 2 in
   let sum = ref 0 in
-  while b < 4000001 do 
-    if b mod 2 = 0 then sum := !sum + b;
+  while !b < 4000001 do 
+    if !b mod 2 = 0 then sum := !sum + !b;
     let tmp = !b in
     b := tmp + !a;
     a := tmp;
