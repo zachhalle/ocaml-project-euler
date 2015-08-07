621f08469a69a1c395d02ca9ef9b8e02142c50fb
Zachary Halle <zhalle@cmu.edu>
2015-08-07 17:27:36 -0400

    modified problem 3

--- problem3.ml
+++ problem3.ml
@@ -20,14 +20,11 @@ let main () =
   let target = ref 600851475143 in
   let i = ref 2 in
   while !i < !target do
-    printf "%d\n" !i;
-    if !target mod !i = 0 && is_prime !i then
-      begin  
-        while !target mod !i = 0 do
-          target := !target / !i
-        done
-      end
-    else i := !i + 1
+    if !target mod !i = 0 && is_prime !i then 
+      while !target mod !i = 0 do
+        target := !target / !i
+      done;
+    i := !i + 1
   done;
   printf "answer %d\n" !i
 
