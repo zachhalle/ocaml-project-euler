4d069484c199125f3311a2543b2b8fb3a6b7226f
Zachary Halle <zhalle@cmu.edu>
2015-08-09 19:52:41 -0400

    finished problem 4

--- problem4.ml
+++ problem4.ml
@@ -1,8 +1,10 @@
+open Printf
+
 let arr_is_pallindrome arr =
   let len = Array.length arr in 
-  let done = len / 2 - 1 in
-  let loop i = 
-    i = done || (arr.(i) = arr.(len - i) && loop (i + 1))
+  let upper = len / 2 in
+  let rec loop i = 
+    i = upper || (arr.(i) = arr.(len - i - 1) && loop (i + 1))
   in
   loop 0
 
@@ -13,7 +15,30 @@ let num_to_arr x =
   in
   Array.of_list (loop x [])
 
-let num_is_pallindrome = arr_is_pallindrome @@ num_to_arr
+let num_is_pallindrome x = arr_is_pallindrome (num_to_arr x)
+
+exception Finished
+
+let rec inner i j = 
+  if j < i 
+  then None
+  else 
+    let ij = i * j in 
+    if num_is_pallindrome ij
+    then Some (ij, i, j)
+    else inner i (j - 1)
+
+let rec outer i = 
+  if i < 100
+  then None
+  else 
+    match inner i 999 with
+    | None -> outer (i - 1)
+    | answer -> answer
 
 let main () =
-  
+  match outer 999 with
+  | None -> printf "Nothing found\n"
+  | Some (x, y, z) -> printf "answer: %d %d %d\n" x y z
+
+let _ = main ()
